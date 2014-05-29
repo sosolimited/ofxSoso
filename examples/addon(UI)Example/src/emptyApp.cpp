87a09a68e02abd655edc78ea6796afb747c401ab
wadeaaron <wade@sosolimited.com>
2014-05-29 17:29:08 -0400

    adding examples/addonExample project

---
+++ examples/addon(UI)Example/src/emptyApp.cpp
@@ -0,0 +1,83 @@
+#include "emptyApp.h"
+
+
+//--------------------------------------------------------------
+void emptyApp::setup(){
+  
+  //Replace the default ofGLRenderer with ofxSosoRenderer which has overriden setupScreen() and setupScreenPerspective().
+	//This lets us set up the scene graph how we want to.
+	//Warning: Up is up and down is down in this world.
+  ofSetCurrentRenderer(ofPtr<ofBaseRenderer>(new ofxSosoRenderer(false)));
+  
+  //Create a scene.
+	//The scene is a scene graph that renders objects added to its root and their children and their children's children and so on.
+	//When the render mode of the scene is set to RENDER_ALPHA_DEPTH_SORTED, it handles sorting of both transparent and opaque objects in the z-axis.
+  scene = new ofxScene(ofGetWidth(), ofGetHeight());
+	scene->setBackgroundColor(10, 10, 10);
+  
+}
+
+//--------------------------------------------------------------
+void emptyApp::update(){
+  
+	//Update the scene with the current time. This call propagates the idle() call to all objects as well.
+	//Note: If you are capturing frames to create a movie, simply replace ofGetElapsedTimef() with a float variable that you increment by a fixed time interval each frame.
+  scene->update(ofGetElapsedTimef());
+}
+
+//--------------------------------------------------------------
+void emptyApp::draw(){
+  
+	//Call draw on scene, which initiates the drawing of the root object.
+  scene->draw();
+}
+
+//--------------------------------------------------------------
+void emptyApp::keyPressed  (int key){
+  
+}
+
+//--------------------------------------------------------------
+void emptyApp::keyReleased(int key){
+  
+}
+
+//--------------------------------------------------------------
+void emptyApp::mouseMoved(int x, int y ){
+  
+}
+
+//--------------------------------------------------------------
+void emptyApp::mouseDragged(int x, int y, int button){
+  
+  
+}
+
+//--------------------------------------------------------------
+void emptyApp::mousePressed(int x, int y, int button){
+  
+}
+
+//--------------------------------------------------------------
+void emptyApp::mouseReleased(int x, int y, int button){
+  
+  
+}
+
+
+//--------------------------------------------------------------
+void emptyApp::windowResized(int w, int h){
+  
+}
+
+//--------------------------------------------------------------
+void emptyApp::gotMessage(ofMessage msg){
+  
+}
+
+//--------------------------------------------------------------
+void emptyApp::dragEvent(ofDragInfo dragInfo){ 
+  
+}
+
+
