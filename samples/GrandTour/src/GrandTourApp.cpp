ef76e49ed65ff273e6cf4047b5925e229db03e5e
David Wicks <wicks.db@gmail.com>
2014-08-25 15:24:43 -0400

    Drawing some stuff in the GrandTour sample.
    The logo and line segments from the previous example.

--- samples/GrandTour/src/GrandTourApp.cpp
+++ samples/GrandTour/src/GrandTourApp.cpp
@@ -1,20 +1,73 @@
 #include "cinder/app/AppNative.h"
 #include "cinder/gl/gl.h"
 
+#include "ofxScene.h"
+#include "ofxRectangleObject.h"
+#include "ofxImageObject.h"
+#include "ofxCircleObject.h"
+#include "ofxAnimation.h"
+#include "ofxLineSegmentObject.h"
+#include "ofxDynamicPolygonObject.h"
+//#include "ofxTextObject.h"
+//#include "ofxLetterTextObject.h"
+
 using namespace ci;
 using namespace ci::app;
 using namespace std;
 
 class GrandTourApp : public AppNative {
-  public:
-	void setup();
-	void mouseDown( MouseEvent event );	
-	void update();
-	void draw();
+public:
+	void prepareSettings( Settings *settings ) override;
+	void setup() override;
+	void mouseDown( MouseEvent event ) override;
+	void update() override;
+	void draw() override;
+private:
+	shared_ptr<ofxScene>								scene;
+	shared_ptr<ofxImageObject>					image;
+	shared_ptr<ofxDynamicPolygonObject> dynamicPolygon;
+
+	shared_ptr<ofxObject>											circleRoot, lineRoot;
+	vector<shared_ptr<ofxCircleObject>>				circles;
+	vector<shared_ptr<ofxLineSegmentObject>>	lines;
+
+	shared_ptr<ofxAnimation>									animation;
 };
+
+void GrandTourApp::prepareSettings( Settings *settings )
+{
+	settings->setWindowSize( 1280, 720 );
+}
 
 void GrandTourApp::setup()
 {
+	scene = make_shared<ofxScene>( getWindowWidth(), getWindowHeight() );
+	scene->setBackgroundColor( 50, 50, 50 );
+
+	image = make_shared<ofxImageObject>( getAssetPath("soso_LOGO.png").string() );
+	image->setSpecialTransparency( true );
+	image->setTrans( getWindowWidth() - image->width - 40, getWindowHeight() - image->height - 40, 5.0f );
+	image->setCentered( false );
+	scene->getRoot()->addChild( image.get() );
+
+	lineRoot = make_shared<ofxObject>();
+	lineRoot->setTrans( 200, 200, 0 );
+	scene->getRoot()->addChild( lineRoot.get() );
+
+	int numLines = 120;
+	for( int i = 0; i < numLines; ++i )
+	{
+		auto line = make_shared<ofxLineSegmentObject>( 2 );
+		line->setVertexPos( 0, 0, 100, 0 );
+		line->setVertexPos( 1, 0, 180, 0 );
+		float gray = 255 - i/(float)numLines * 200;
+		line->setColor( ColorA8u::gray( gray ) );
+		line->setTrans( 0, 0, 0 );
+		line->setLineWidth( 2.0 );
+		line->setRot( 0, 0, gray );
+		lines.push_back( line );
+		lineRoot->addChild( line.get() );
+	}
 }
 
 void GrandTourApp::mouseDown( MouseEvent event )
@@ -23,12 +76,21 @@ void GrandTourApp::mouseDown( MouseEvent event )
 
 void GrandTourApp::update()
 {
+	scene->update( getElapsedSeconds() );
 }
 
 void GrandTourApp::draw()
 {
 	// clear out the window with black
-	gl::clear( Color( 0, 0, 0 ) ); 
+	gl::setMatricesWindowPersp( getWindowSize() );
+	gl::setViewport( getWindowBounds() );
+
+	scene->draw();
+/*
+	gl::setMatricesWindowPersp( getWindowSize() );
+	gl::translate( Vec2f( 0.0f, 200.0f ) );
+	gl::draw( mTexture );
+*/
 }
 
 CINDER_APP_NATIVE( GrandTourApp, RendererGl )
