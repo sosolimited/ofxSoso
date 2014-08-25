36179e3f818b66bd27e1e8b58185361168d1b638
David Wicks <wicks.db@gmail.com>
2014-08-25 15:53:30 -0400

    Dynamic polygon animation copied over.

--- samples/GrandTour/src/GrandTourApp.cpp
+++ samples/GrandTour/src/GrandTourApp.cpp
@@ -29,6 +29,7 @@ private:
 	shared_ptr<ofxScene>								scene;
 	shared_ptr<ofxImageObject>					image;
 	shared_ptr<ofxDynamicPolygonObject> dynamicPolygon;
+	gl::TextureRef											polyTex;
 
 	shared_ptr<ofxObject>											circleRoot, lineRoot;
 	vector<shared_ptr<ofxCircleObject>>				circles;
@@ -65,6 +66,34 @@ void GrandTourApp::setup()
 
 	//_________________________________________________________________________________________________________________
 
+	// Create a textured dynamic polygon object.
+	// See how it is animated below in keyPressed().
+	polyTex = gl::Texture::create( loadImage( loadAsset( "plasticman.jpg" ) ) );
+
+	dynamicPolygon = make_shared<ofxDynamicPolygonObject>(4);
+	dynamicPolygon->enableVertexColoring( false );
+	dynamicPolygon->setTrans( 250, 70, 0 );
+	dynamicPolygon->setScale( 0.5f );
+	dynamicPolygon->setTexture( polyTex );
+
+	//Set the home positions and tex coords of the vertices.
+	//Note: We start at the lower left corner and move around counter clockwise, as a general practice.
+	dynamicPolygon->setVertexPos(0, Vec3f(0, 0.3*polyTex->getHeight(), 0));
+	dynamicPolygon->setVertexTexCoords(0, 0, 0.3f );
+	//
+	dynamicPolygon->setVertexPos(1, Vec3f(polyTex->getWidth(), 0.3*polyTex->getHeight(), 0));
+	dynamicPolygon->setVertexTexCoords(1, 1.0f, 0.3f );
+	//
+	dynamicPolygon->setVertexPos(2, Vec3f(polyTex->getWidth(), 0.7*polyTex->getHeight(), 0));
+	dynamicPolygon->setVertexTexCoords(2, 1.0f, 0.7f );
+	//
+	dynamicPolygon->setVertexPos(3, Vec3f(0, 0.7*polyTex->getHeight(), 0));
+	dynamicPolygon->setVertexTexCoords(3, 0, 0.7f );
+
+	scene->getRoot()->addChild( dynamicPolygon.get() );								//Add the polygon to the scene.
+
+	//_________________________________________________________________________________________________________________
+
 	// Make and lay out some lines. See how they are animated below in keyPressed().
 	lineRoot = make_shared<ofxObject>();
 	lineRoot->setTrans( 200, 200, 0 );
@@ -80,7 +109,7 @@ void GrandTourApp::setup()
 		line->setColor( ColorA8u::gray( gray ) );
 		line->setTrans( 0, 0, 0 );
 		line->setLineWidth( 2.0 );
-		line->setRot( 0, 0, gray );
+		line->setRot( 0, 0, 0 );
 		lines.push_back( line );
 		lineRoot->addChild( line.get() );
 	}
@@ -121,9 +150,42 @@ void GrandTourApp::keyDown( KeyEvent event )
 				}
 			}
 		break;
+		case KeyEvent::KEY_DOWN:
+		{
+			float dur = 1.0;
 
+			dynamicPolygon->gotoVertexPos(0, Vec3f(0, 0.3*polyTex->getHeight(), 0), dur);
+			dynamicPolygon->gotoVertexTexCoords(0, 0, 0.3, dur);
+
+			dynamicPolygon->gotoVertexPos(1, Vec3f(polyTex->getWidth(), 0.3*polyTex->getHeight(), 0), dur);
+			dynamicPolygon->gotoVertexTexCoords(1, 1.0, 0.3, dur);
+
+			dynamicPolygon->gotoVertexPos(2, Vec3f(polyTex->getWidth(), 0.7*polyTex->getHeight(), 0), dur);
+			dynamicPolygon->gotoVertexTexCoords(2, 1.0, 0.7, dur);
+
+			dynamicPolygon->gotoVertexPos(3, Vec3f(0, 0.7*polyTex->getHeight(), 0), dur);
+			dynamicPolygon->gotoVertexTexCoords(3, 0, 0.7, dur);
+		}
+		break;
+		case KeyEvent::KEY_UP:
+		{
+			float dur = 1.0;
+
+			dynamicPolygon->gotoVertexPos(0, Vec3f(0, 0, 0), dur);
+			dynamicPolygon->gotoVertexTexCoords(0, 0, 0.0, dur);
+
+			dynamicPolygon->gotoVertexPos(1, Vec3f(polyTex->getWidth(), 0, 0), dur);
+			dynamicPolygon->gotoVertexTexCoords(1, 1.0, 0.0, dur);
+
+			dynamicPolygon->gotoVertexPos(2, Vec3f(polyTex->getWidth(), polyTex->getHeight(), 0), dur);
+			dynamicPolygon->gotoVertexTexCoords(2, 1.0, 1.0, dur);
+
+			dynamicPolygon->gotoVertexPos(3, Vec3f(0, polyTex->getHeight(), 0), dur);
+			dynamicPolygon->gotoVertexTexCoords(3, 0, 1.0, dur);
+		}
+		break;
 		default:
-			break;
+		break;
 	}
 }
 
@@ -139,11 +201,7 @@ void GrandTourApp::draw()
 	gl::setViewport( getWindowBounds() );
 
 	scene->draw();
-/*
-	gl::setMatricesWindowPersp( getWindowSize() );
-	gl::translate( Vec2f( 0.0f, 200.0f ) );
-	gl::draw( mTexture );
-*/
+
 }
 
 CINDER_APP_NATIVE( GrandTourApp, RendererGl )
